a4f143b91e3144eacb5795acf38d08e0b3f2143c
Chloe Spices <45343924+HabaneroSpices@users.noreply.github.com>
2020-06-19 23:23:43 +0200

    Update docker-ce.sh

--- installer/docker-ce.sh
+++ installer/docker-ce.sh
@@ -1,53 +1,31 @@
 #!/bin/bash
 #
 
-install_debian() {
+install() {
     sudo -v
-    echo '-------------'
-    echo 'Removing old docker components...'
-    echo '-------------'
-
-    sudo apt-get remove -y docker docker-engine docker.io containerd runc
-
-    echo '-------------'
-    echo 'Installing prerequisites...'
-    echo '-------------'
-
-    sudo apt-get update
-
-    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-
-    echo '-------------'
-    echo 'Adding docker repo...'
-    echo '-------------'
-
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-    sudo apt-key fingerprint 0EBFCD88
-
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-    echo '-------------'
-    echo 'Installing docker...'
-    echo '-------------'
-
-    sudo apt-get update
-
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    echo '[*] Downloading...'
+    curl -fsSL https://get.docker.com -o /tmp/get-docker.sh.tmp
+    echo '[*] Installing...'
+    sudo sh /tmp/get-docker.sh.tmp
+    echo '[*] Cleaning up...'
+    sudo rm -f /tmp/get-docker.sh
+    echo '[!] Run docker without root?'
+    read -p '[Y/n]: ' choice
+    if [! '$choice' -eq Y ]; then exit;
+    sudo usermod -aG docker $USER
 }
 
 case $1 in
 
-apt)
-    echo "Installing..."
-    install_debian
+easy)
+    install
     echo "Done."
     ;;
-yum)
+fuck)
     echo "Asshole. ouff"
     ;;
 *)
-    echo "Usage... $0 (apt|yum)"
+    echo "Usage... $0 (easy|fuck)"
     ;;
 
 esac
