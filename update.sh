9c78a73ab5d9473f60d869edc8aa827b93cb9936
gooner <gooner@rkbweb.goon.local>
2024-05-03 07:43:58 +0000

    Modified

--- update.sh
+++ update.sh
@@ -9,6 +9,16 @@
 #
 function main() {
 	cd $DIR
+
+	if [ "$CRON" = true ]; then
+		isCron=$()
+		if crontab -l | grep $DIR/update.sh > /dev/null; then
+		log "Cronjob for `pwd`/update.sh already configured"
+		else
+		crontab -l | { cat; echo "* * * * * /usr/bin/bash `pwd`/update.sh"; } | crontab - && log "Added cronjob for `pwd`/update.sh" || log "Could not add cronjob for `pwd`/update.sh"
+		fi
+	fi
+
 	log "Fetching remote repository..."
 	git fetch || exit 1
 
@@ -45,23 +55,27 @@ function log() {
 function on-exit() {
 	rm "$lockfile"
 }
+function usage() {
+      echo "Usage: ${SCRIPT_NAME}... [--cron]"
+}
+function parse-opts() {
+	CRON=false
+	[[ "${*}" == *"--cron"* ]] && CRON=true
+}
 
 # Execution
-
-set -e
-set -o pipefail
 set -C
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 DIR_NAME=$(basename "${DIR}")
 SCRIPT_NAME=$(basename "${0}")
 
-trap on-exit SIGINT SIGTERM EXIT ERR
+parse-opts "${@}"
 
+trap on-exit SIGINT SIGTERM EXIT ERR
 lockfile="/tmp/${DIR_NAME}.lock"
 if echo "$$" >"$lockfile"; then
 	log "Successfully acquired lock"
-	main "${@}"
+	main
 else
 	log "Cannot acquire lock - already locked by $(cat "$lockfile")"
 	exit 1
